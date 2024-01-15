a2281cbd4eae63ed6cd32c94df1b85d5f3bb2c7e
Gordon Cheung <cheungcheukheigordon@gmail.com>
2024-01-15 22:43:19 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info); 
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World!");
+    loop {}
+}
+
+mod vga_buffer;
